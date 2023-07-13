164667ec74d51f930f0a138290378923d718eb68
MGabashvili <68913100+MGabashvili@users.noreply.github.com>
2023-07-13 23:25:08 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [show, setShow] = useState('true');
+  console.log(typeof show);
+	return (
+		<>
+			<p>show: {show.toString()}</p>
+			<button onClick={() => setShow(true)}>Show</button>
+			<button onClick={() => setShow(false)}>Hide</button>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/product.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Product(asdasd) {
+	return (
+		<div>
+			<button onClick={asdasd.handler}></button>
+			<p>{asdasd.name}</p>
+			<p>{asdasd.price}</p>
+			<p>{asdasd.rating}</p>
+		</div>
+	);
+}
